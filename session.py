8bba741352ce02b343dda7538f2806f11c94934f
Paul Anholt <panholt@cisco.com>
2017-08-11 15:23:23 -0400

    More methods

--- session.py
+++ session.py
@@ -1,5 +1,4 @@
 import os
-from collections import deque
 from time import sleep
 
 import requests
--- utils/time.py
+++ utils/time.py
@@ -3,3 +3,9 @@ from datetime import datetime
 
 def ts_to_dt(ts):
     return datetime.strptime(ts, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+def dt_to_ts(dt=None):
+    if not dt:
+        dt = datetime.now()
+    assert isinstance(dt, datetime)
+    return dt.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
