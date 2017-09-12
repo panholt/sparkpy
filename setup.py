276c67759047b595e73d9627f6be7122a891cbbd
Paul Anholt <panholt@cisco.com>
2017-09-12 11:52:57 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='SparkPy',
+    version='0.1dev',
+    packages=['sparkpy',],
+    license='MIT',
+    long_description=open('README.rst').read(),
+)
