9ccc51f4f10a4f285cdc1dc2a7b738aa745e5620
Paul Anholt <panholt@cisco.com>
2017-09-12 12:47:56 -0400

    setup.py fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name='SparkPy',
     version='0.1dev',
-    packages=['sparkpy',],
-    license='MIT',
+    packages=['sparkpy'],
+    license='MIT License',
+    homepage='https://github.com/panholt/sparkpy',
     long_description=open('README.rst').read(),
 )
