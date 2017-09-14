359e9359cd320b832de2340a74a895065ae17339
Paul Anholt <panholt@gmail.com>
2017-09-14 19:50:16 -0400

    setup improvements

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from distutils.core import setup
 
 setup(
     name='sparkpy',
+    author='Paul Anholt',
+    author_email='panholt@gmail.com',
     version='0.1dev',
-    packages=['sparkpy'],
+    packages=['sparkpy', 'sparkpy.models'],
     license='MIT',
     url='https://github.com/panholt/sparkpy',
     long_description=open('README.rst').read(),
