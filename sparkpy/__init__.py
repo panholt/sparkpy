6fccc3416a5d306f558b22888c8a58338d57482c
Paul Anholt <panholt@gmail.com>
2017-09-23 11:13:50 -0400

    remove console logging

--- sparkpy/__init__.py
+++ sparkpy/__init__.py
@@ -38,12 +38,3 @@ logging.basicConfig(level=logging.DEBUG,
                     datefmt='%m-%d %H:%M',
                     filename='/tmp/sparkpy.log',
                     filemode='w')
-# define a Handler which writes INFO messages or higher to the sys.stderr
-console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
-# set a format which is simpler for console use
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-# tell the handler to use this format
-console.setFormatter(formatter)
-# add the handler to the root logger
-logging.getLogger('').addHandler(console)
