dcd2416217a7aaf649b785f905c3a5da2d6f7656
Paul Anholt <panholt@gmail.com>
2017-09-23 11:14:29 -0400

    remove console logging

--- sparkpy/__init__.py
+++ sparkpy/__init__.py
@@ -30,11 +30,3 @@ __all__ = ['Spark',
            'SparkTeam',
            'SparkTeamMembership',
            'SparkWebhook']
-
-
-# set up logging to file - see previous section for more details
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M',
-                    filename='/tmp/sparkpy.log',
-                    filemode='w')
